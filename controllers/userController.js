77132646c40e0124f90e0ee7f0129580ccebdf50
Joanna Christian <joanna.s.christian@gmail.com>
2022-04-16 22:02:18 -0700

    initial commit

---
+++ controllers/userController.js
@@ -0,0 +1,24 @@
+const { User, Thought} = require('../models');
+
+module.exports = {
+// GET all users
+getUsers(req, res) {
+    User.find
+}
+
+
+// GET a single user by its _id and populated thought and friend data
+
+
+// POST a new user
+
+
+// PUT to update a user by its _id
+
+
+// DELETE to remove user by its _id
+
+
+// Bonus: Remove a user's associated thoughts when deleted
+    
+}
